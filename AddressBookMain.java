07ea4179abcbdf0654e3ee06c2bfe8d3bf6e112a
ishuks9 <ishuks9@gmail.com>
2021-07-11 20:06:45 +0530

    updated the uc2

---
+++ AddressBookMain.java
@@ -0,0 +1,13 @@
+public class AddressBookMain {
+
+      public static void main(String arg[]){
+
+              System.out.println("Welcome to Address Book");
+
+                AddressBookManager addressBook = new AddressBookManager();
+
+                addressBook.print();
+        }
+
+}
+
