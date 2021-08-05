61ab0dd5925cbe84fc82246e35a19fd12f779e10
ishuks9 <ishuks9@gmail.com>
2021-08-05 16:55:18 +0530

    updated the UC3

--- Person.java
+++ Person.java
@@ -1,13 +1,12 @@
 import java.util.*;
 import java.io.*;
 
-public class Person{
-	
+public class Person {
 	// Class-variable declarations
 	private String name, city, state, address, zipCode;
 	private long phoneNumber;
-	
-	public Person(String name, String city, String state, String address, String zipCode, long phoneNumber){
+
+	public Person(String name, String city, String state, String address, String zipCode, long phoneNumber) {
 		this.name = name;
 		this.city = city;
 		this.state = state;
@@ -19,52 +18,60 @@ public class Person{
 	// Methods for modifying class-variables
 
 	/* Note: Modifying name of a Person is not allowed. */
-	public void setCity(String city){
+	public void setCity(String city) {
 		this.city = city;
 	}
-	public void setState(String state){
+
+	public void setState(String state) {
 		this.state = state;
 	}
-	public void setAddress(String address){
+
+	public void setAddress(String address) {
 		this.address = address;
 	}
-	public void setZipCode(String zipCode){
+
+	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
-	public void setPhoneNumber(long phoneNumber){
+
+	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
 	// Methods for displaying class-variables
-	public String getName(){
+	public String getName() {
 		return this.name;
 	}
-	public String getCity(){
+
+	public String getCity() {
 		return this.city;
 	}
-	public String getState(){
+
+	public String getState() {
 		return this.state;
 	}
-	public String getAddress(){
+
+	public String getAddress() {
 		return this.address;
 	}
-	public String getZipCode(){
+
+	public String getZipCode() {
 		return this.zipCode;
 	}
-	public long getPhoneNumber(){
+
+	public long getPhoneNumber() {
 		return this.phoneNumber;
 	}
 
-             public void showPersonsDetails(){
+	public void showPersonsDetails() {
 
-		System.out.println("\n+" + new String(new char[54]).replace('\0', '-')+"+");
-		System.out.format("| %20s: %30s |\n","Name", this.getName());
-		System.out.format("| %20s: %30s |\n","Phone Number", this.getPhoneNumber());
-		System.out.format("| %20s: %30s |\n","City", this.getCity());
-		System.out.format("| %20s: %30s |\n","State", this.getState());
-		System.out.format("| %20s: %30s |\n","Address", this.getAddress());
-		System.out.format("| %20s: %30s |\n","Zip Code", this.getZipCode());
-		System.out.println("+" + new String(new char[54]).replace('\0', '-')+"+");
+		System.out.println("\n+" + new String(new char[54]).replace('\0', '-') + "+");
+		System.out.format("| %20s: %30s |\n", "Name", this.getName());
+		System.out.format("| %20s: %30s |\n", "Phone Number", this.getPhoneNumber());
+		System.out.format("| %20s: %30s |\n", "City", this.getCity());
+		System.out.format("| %20s: %30s |\n", "State", this.getState());
+		System.out.format("| %20s: %30s |\n", "Address", this.getAddress());
+		System.out.format("| %20s: %30s |\n", "Zip Code", this.getZipCode());
+		System.out.println("+" + new String(new char[54]).replace('\0', '-') + "+");
 	}
-    
 }
